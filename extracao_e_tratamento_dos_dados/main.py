5b1f179c21d9bffbb103cdde57b399b8e960e073
Marcelo Allan de Melo Silva <marceloallan_@hotmail.com>
2025-03-31 16:14:41 -0400

    Adicionando as tabelas do word no código HTML
    Implementando código para identificar as tabelas no arquivo work, convertê-las para HTML e salva-las em um arquivo no DF de parágrafos com a Class tabela. Posteriormente essas tabelas são escritas no código HTML dentro de uma DIV

--- extracao_e_tratamento_dos_dados/main.py
+++ extracao_e_tratamento_dos_dados/main.py
@@ -5,63 +5,73 @@ import re
 def validate_data(data, pattern):
     return bool(re.match(pattern, data, re.IGNORECASE))
 
-def classify_paragraph(text):
+def classify_paragraph(text, pos_no_doc):
     match text:
         case text_paragraph if validate_data(text_paragraph, pattern_capitulo):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'capitulo'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_secao):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'secao'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_subsecao):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'subsecao'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_titulo_capitulo_secao_ou_subsecao):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'titulo-capitulo-secao-ou-subsecao'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_titulo):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'titulo'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_livro):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'livro'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_artigo):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'artigo'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_alinea):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'alinea'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_inciso):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'inciso'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_paragrafo):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'paragrafo'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
@@ -69,47 +79,70 @@ def classify_paragraph(text):
 
 
         case text_paragraph if validate_data(text_paragraph, pattern_item):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'item'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_paragrafo_em_branco):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'paragrafo-em-branco'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_paragrafo_vazio):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'paragrafo-vazio'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_nota_explicativa):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'nota-explicativa'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_nota_de_rodape):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'nota-de-rodape'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
         case text_paragraph if validate_data(text_paragraph, pattern_item_nota_de_rodape):
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'item-nota-de-rodape'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
+        case text_paragraph if validate_data(text_paragraph, pattern_tabela):
+            paragraph_classified['Index'] = pos_no_doc
+            paragraph_classified['Class'] = 'tabela'
+            paragraph_classified['Content'] = text_paragraph
+            paragraphs_RICMS.append(paragraph_classified)
+            return
+
         case text_paragraph:
+            paragraph_classified['Index'] = pos_no_doc
             paragraph_classified['Class'] = 'nao-classificado'
             paragraph_classified['Content'] = text_paragraph
             paragraphs_RICMS.append(paragraph_classified)
             return
 
+def table_to_html(table):
+    # Converter a tabela em um DataFrame
+    data = [[cell.text.strip() for cell in row.cells] for row in table.rows]
+    df = pd.DataFrame(data)
+
+    # Converter DataFrame para HTML
+    html_table = df.to_html(index=False, header=False, escape=False)
+
+    return html_table
 
 pattern_artigo = r'^\s?Art\.\s?'
 pattern_artigo_com_numero = r'Art\.\s\d+\°?(-[A-Z])?'
@@ -127,6 +160,7 @@ pattern_nota_explicativa = r'^\s*Nota\sexplicativa'
 pattern_nota_de_rodape = r'^\s*Notas?:'
 pattern_item_nota_de_rodape = r'^\d+-?[A-Z]?\.'
 pattern_paragrafo_em_branco = r'^\s*$'
+pattern_tabela = r'^<table'
 pattern_paragrafo_vazio = r'^.$'
 
 paragraphs_RICMS = list()
@@ -136,17 +170,26 @@ paragraph_classified = dict()
 doc = Document('data/RICMS-Corrido.docx')
 
 # classificar os parágrafos
-for paragraph in doc.paragraphs[:10500]:  # percorre todos os parágrafos do documento
-    paragraph_classified = dict()  # cria um novo dicionário para guardar a classificação do parágrafo
+for index, elemento in enumerate(doc.element.body):
+    if elemento.tag.endswith('p'):  # verifica se o elemento é um parágrafo
+        paragraph_classified = dict()  # cria um novo dicionário para guardar a classificação do parágrafo
 
-    content_paragraph = paragraph.text.strip()
+        content_paragraph = elemento.text.strip()
 
-    # substitui os símbolos <, > e & pelo html correspondente para evitar erros na sintaxe
-    content_paragraph = re.sub('<', '&lt;', content_paragraph)
-    content_paragraph = re.sub('>', '&gt;', content_paragraph)
-    content_paragraph = re.sub('&', '&amp;', content_paragraph)
+        # substitui os símbolos <, > e & pelo html correspondente para evitar erros na sintaxe
+        content_paragraph = re.sub('<', '&lt;', content_paragraph)
+        content_paragraph = re.sub('>', '&gt;', content_paragraph)
+        content_paragraph = re.sub('&', '&amp;', content_paragraph)
 
-    classify_paragraph(content_paragraph)  # classifica o parágrafo a partir das expressões regulares
+        classify_paragraph(content_paragraph, index)  # classifica o parágrafo a partir das expressões regulares
+
+    elif elemento.tag.endswith('tbl'):
+        paragraph_classified = dict()   # cria um novo dicionário para guardar a classificação do parágrafo
+
+        table_index = sum(1 for e in doc.element.body[:index] if e.tag.endswith('tbl'))  # identifica o índice dessa tabela na lista doc.tables
+        table_obj = doc.tables[table_index]  # retorna a tabela correta
+
+        classify_paragraph(table_to_html(table_obj), index)
 
 # criar DataFrame e salvar em Excel
 df_paragraphs_RICMS = pd.DataFrame(paragraphs_RICMS)
@@ -171,9 +214,11 @@ html_tags_for_class_paragraph = {  # lista a TAG que será atribuída a cada cla
     'titulo': 'h2',
     'subsecao': 'h3',
     'livro': 'h1',
+    'tabela': 'div',
     'nao-classificado': 'p'
 }
 
+# início do código HTML
 html_content = """<!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -183,7 +228,15 @@ html_content = """<!DOCTYPE html>
 </head>
 <body>
     <header class="cabecalho">
-    
+        <h1>Secretaria de Estado de Fazenda de Mato Grosso</h1>
+        <nav>
+            <ul class="menu">
+                <li><a href="https://www5.sefaz.mt.gov.br/inicio" target="_blank">Site Oficial</a></li>
+                <li><a href="https://www.sefaz.mt.gov.br/spl/portalpaginalegislacao" target="_blank">Portal da Legislação</a></li>
+                <li><a href="https://portal.mt.gov.br/" target="_blank">Portal Gov MT</a></li>
+            </ul>
+        </nav>
+        <div class="barra-degrade"></div>
     </header>
     <main>
 """
@@ -202,6 +255,7 @@ for index, row in df_paragraphs_RICMS.iterrows():
 html_content += """
     </main>
 <footer>
+    <p>Site em desenvolvimento por Marcelo Allan - FTE da SEFAZ/MT</p>
 </footer>
 </body>
 </html>"""
